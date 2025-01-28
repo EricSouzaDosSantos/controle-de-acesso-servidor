558a70b76127142f195bd891a7f840f758eb5738
Eric <ericsouza94895@gmail.com>
2025-01-27 23:35:18 -0300

    refactor: deletando as listas(não serão mais utilizadas) e passando os métodos agurdarIdACesso e cadastrarIdAcesso para a classe usuário

--- src/main/java/com/senai/controledeacesso/ControleDeAcesso.java
+++ src/main/java/com/senai/controledeacesso/ControleDeAcesso.java
@@ -12,9 +12,6 @@ public class ControleDeAcesso {
     // Caminho para o arquivo bancoDeDados.txt e para a pasta imagens
     public static final File pastaImagens = gerenciarArquivo.getArquivoImagens();
 
-    static String[][] matrizCadastro = {{"", ""}};
-    public static String[][] matrizRegistrosDeAcesso = {{"", "", ""}};// inicia a matriz com uma linha e duas colunas com "" para que na primeira vez não apareça null na tabela de registros
-
     static volatile boolean modoCadastrarIdAcesso = false;
     static int idUsuarioRecebidoPorHTTP = 0;
     static String dispositivoRecebidoPorHTTP = "Disp1";
@@ -80,7 +77,7 @@ public class ControleDeAcesso {
                     RegistroDeAcesso.exibirAcesso(scanner);
                     break;
                 case 6:
-                    aguardarCadastroDeIdAcesso();
+                    Usuario.aguardarCadastroDeIdAcesso();
                     break;
                 case 7:
                     System.out.println("Fim do programa!");
@@ -92,79 +89,16 @@ public class ControleDeAcesso {
         } while (opcao != 7);
     }
 
-    private static void aguardarCadastroDeIdAcesso() {
-        modoCadastrarIdAcesso = true;
-        System.out.println("Aguardando nova tag ou cartão para associar ao usuário");
-        // Usar Future para aguardar até que o cadastro de ID seja concluído
-        Future<?> future = executorCadastroIdAcesso.submit(() -> {
-            while (modoCadastrarIdAcesso) {
-                // Loop em execução enquanto o modoCadastrarIdAcesso estiver ativo
-                try {
-                    Thread.sleep(100); // Evita uso excessivo de CPU
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-        });
-
-        try {
-            future.get(); // Espera até que o cadastro termine
-        } catch (Exception e) {
-            System.err.println("Erro ao aguardar cadastro: " + e.getMessage());
-        }
-    }
-
     private static void processarMensagemMQTTRecebida(String mensagem) {
         System.out.println("mensagem recebida: "+mensagem);
         if (!modoCadastrarIdAcesso) {
             executorIdentificarAcessos.submit(() -> RegistroDeAcesso.criarNovoRegistroDeAcesso(mensagem, Usuario.getListaUsuarios())); // Processa em thread separada
         } else {
-            cadastrarNovoIdAcesso(mensagem); // Processa em thread separada
+            Usuario.cadastrarNovoIdAcesso(mensagem, scanner); // Processa em thread separada
             modoCadastrarIdAcesso = false;
             idUsuarioRecebidoPorHTTP = 0;
         }
     }
 
-    private static void cadastrarNovoIdAcesso(String novoIdAcesso) {
-        boolean encontrado = false;
-        String idUsuarioEscolhido = String.valueOf(idUsuarioRecebidoPorHTTP);
-        String dispositivoEscolhido = dispositivoRecebidoPorHTTP;
 
-        List<Usuario> listaDeUsuarios = Usuario.getListaUsuarios();
-
-
-        if (idUsuarioRecebidoPorHTTP == 0) {
-            for (Usuario usuario : listaDeUsuarios) {
-                System.out.println(usuario.getId() + " - " + usuario.getNome());
-            }
-            System.out.print("Digite o ID do usuário para associar ao novo idAcesso: ");
-            idUsuarioEscolhido = scanner.nextLine();
-            conexaoMQTT.publicarMensagem(topico, dispositivoEscolhido);
-            System.out.println("publicou a mensagem");
-        }
-
-        modoCadastrarIdAcesso = true;
-        System.out.println("saiu do publicar");
-
-        for(Usuario usuario: listaDeUsuarios){
-            System.out.println("Comparando id");
-            System.out.println("novoIdAcesso: "+novoIdAcesso);
-            if(usuario.getId() == Integer.parseInt(idUsuarioEscolhido)){
-                System.out.println("vai pegar o uuid");
-
-                usuario.setIdAcesso(UUID.fromString(novoIdAcesso));
-                System.out.println("Pegou o uuid");
-                System.out.println("id de acesso " + novoIdAcesso + " associado ao usuário " + usuario.getNome());
-                conexaoMQTT.publicarMensagem("cadastro/disp", "CadastroConcluido");
-                encontrado = true;
-                Usuario.inserirUsuariosNoArquivo(gerenciarArquivo.getArquivoBancoDeDados());
-                break;
-            }
-            System.out.println("Comparou");
-        }
-        // Se não encontrou o usuário, imprime uma mensagem
-        if (!encontrado) {
-            System.out.println("Usuário com id" + idUsuarioEscolhido + " não encontrado.");
-        }
-    }
 }
